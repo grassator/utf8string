8d965efb375d4f66b5e4f777ecb3df859867241d
Dmitriy Kubyshkin <grassator@gmail.com>
2016-03-10 22:54:12 +0100

    Added `substring` method

--- test/modifying.spec.js
+++ test/modifying.spec.js
@@ -43,6 +43,56 @@ describe('Modification', function () {
 
     });
 
+    describe('substring', function () {
+
+        it('should clone a string when called without any arguments', function () {
+            const str = new Utf8String('Язь');
+            assert.deepStrictEqual(String(str.substring()), 'Язь');
+        });
+
+        it('should clone to the end of the string when given only one iterator', function () {
+            const str = new Utf8String('Язь');
+            const iter = str.codePoints();
+            iter.next();
+            assert.deepStrictEqual(String(str.substring(iter)), 'зь');
+            iter.next();
+            assert.deepStrictEqual(String(str.substring(iter)), 'ь');
+            iter.next();
+            assert.deepStrictEqual(String(str.substring(iter)), '');
+        });
+
+        it('should work with start and end iterators', function () {
+            const str = new Utf8String('Язь');
+            const startIter = str.codePoints();
+            const endIter = Utf8String.advance(startIter.clone(), 2);
+            assert.deepStrictEqual(
+                String(str.substring(startIter, endIter)),
+                'Яз'
+            );
+            startIter.next();
+            endIter.next();
+            assert.deepStrictEqual(
+                String(str.substring(startIter, endIter)),
+                'зь'
+            );
+        });
+
+        it('should throw when called not with a CodePointsIterator', function () {
+            const str = new Utf8String('Язь');
+            assert.throws(() => str.substring('foo'), RangeError);
+            assert.throws(() => str.substring(str.codePoints(), 'foo'), RangeError);
+        });
+
+        it('should throw when the end iterator is before the start one', function () {
+            const str = new Utf8String('Язь');
+            const startIter = str.codePoints();
+            const endIter = str.codePoints();
+            startIter.next();
+            assert.throws(() => str.substring(startIter, endIter), RangeError);
+        });
+
+    });
+
 });
 
 
